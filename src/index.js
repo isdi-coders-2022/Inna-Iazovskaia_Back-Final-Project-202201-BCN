9cffa2ba2a6408136923ea279a51b7e2029f022a
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-08 13:45:27 +0100

    Imported connectDatabase into src/index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ const debug = require("debug")("findme:root");
 const chalk = require("chalk");
 const uppServer = require("./server/uppServer");
 const app = require("./server/index");
+const connectDatabase = require("./database/index");
 
 const port = process.env.PORT || 4000;
+const connectionString = process.env.DATABASE_STRING;
 
 (async () => {
   try {
+    await connectDatabase(connectionString);
     await uppServer(port, app);
   } catch (error) {
     debug(chalk.redBright(`Error: ${error.message}`));
