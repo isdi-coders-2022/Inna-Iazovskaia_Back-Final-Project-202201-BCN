ec0589b8506abfd1ee7eab04c6bec7b1604b3e22
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-08 23:30:50 +0100

    Created getAllMessages controller in messagesControllers.js

---
+++ src/server/controllers/messagesControllers.js
@@ -0,0 +1,8 @@
+const Message = require("../../database/models/Message");
+
+const getAllMessages = async (req, res) => {
+  const messages = await Message.find();
+  res.json({ messages });
+};
+
+module.exports = getAllMessages;
