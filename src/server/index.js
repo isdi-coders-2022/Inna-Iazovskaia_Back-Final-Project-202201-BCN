a54adb82b7a3725f96b9be47daf536ae690baa66
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-09 12:20:05 +0100

    Added middlewares and imported messageRouters in src/server/index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const morgan = require("morgan");
+
+const cors = this.require("cors");
 const helmet = require("helmet");
+const { generalError, notFoundError } = require("./middlewares/errors");
+const messagesRouter = require("./routers/messagesRouters");
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
+app.use(express.json);
 app.use(helmet());
+
+app.use("/messages", messagesRouter);
+
+app.use(generalError);
+app.use(notFoundError);
 
 module.exports = app;
