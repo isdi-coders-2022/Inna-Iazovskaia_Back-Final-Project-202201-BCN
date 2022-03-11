aed6df03bb1c9eb9096efe850297619fd18ff7f5
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-11 14:14:01 +0100

    Created deleteMessage controller in messagesControllers.js

--- src/server/controllers/messagesControllers.js
+++ src/server/controllers/messagesControllers.js
@@ -5,4 +5,20 @@ const getAllMessages = async (req, res) => {
   res.json({ messages });
 };
 
-module.exports = { getAllMessages };
+const deleteMessage = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const messageToDalete = await Message.findByIdAndDelete(id);
+    if (messageToDalete) {
+      res.status(200).json(messageToDalete);
+    } else {
+      const error = new Error("Message not found");
+      error.code = 404;
+      next(error);
+    }
+  } catch (error) {
+    error.message = "Message not deleted";
+    next(error);
+  }
+};
+module.exports = { getAllMessages, deleteMessage };
