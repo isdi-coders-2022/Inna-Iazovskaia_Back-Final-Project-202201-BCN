2f118f6f689a40a3ac123f32575e334f93fbb04a
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-13 18:53:41 +0100

    Created /create endpoint in messagesRouters.js

--- src/server/routers/messagesRouters.js
+++ src/server/routers/messagesRouters.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   getAllMessages,
   deleteMessage,
+  createMessage,
 } = require("../controllers/messagesControllers");
 
 const router = express.Router();
 
 router.get("/all", getAllMessages);
 router.delete("/delete/:id", deleteMessage);
+router.post("/create", createMessage);
 
 module.exports = router;
