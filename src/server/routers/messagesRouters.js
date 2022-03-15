d6b9c73a6f9aec830e3e87233ba17086f5bbd92c
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-15 14:52:22 +0100

    Created /update/:id endpoint in messagesRouters.js

--- src/server/routers/messagesRouters.js
+++ src/server/routers/messagesRouters.js
@@ -3,6 +3,7 @@ const {
   getAllMessages,
   deleteMessage,
   createMessage,
+  updateMessage,
 } = require("../controllers/messagesControllers");
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/all", getAllMessages);
 router.delete("/delete/:id", deleteMessage);
 router.post("/create", createMessage);
+router.put("/update/:id", updateMessage);
 
 module.exports = router;
