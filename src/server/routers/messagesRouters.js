b21274a4a157417aeac85295dea743befc71ed7c
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-17 15:05:17 +0100

    Imported messageValidation into messagesRouters.js

--- src/server/routers/messagesRouters.js
+++ src/server/routers/messagesRouters.js
@@ -1,16 +1,18 @@
 const express = require("express");
+const { validate } = require("express-validation");
 const {
   getAllMessages,
   deleteMessage,
   createMessage,
   updateMessage,
 } = require("../controllers/messagesControllers");
+const messageValidation = require("../controllers/messageValidation");
 
 const router = express.Router();
 
 router.get("/all", getAllMessages);
 router.delete("/delete/:id", deleteMessage);
-router.post("/create", createMessage);
+router.post("/create", validate(messageValidation), createMessage);
 router.put("/update/:id", updateMessage);
 
 module.exports = router;
