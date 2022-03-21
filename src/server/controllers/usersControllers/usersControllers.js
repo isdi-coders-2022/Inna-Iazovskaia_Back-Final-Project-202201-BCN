e1b091f828ed011860d5382b43da4eadcb0768db
Inna Iazovkaia <inna.0961@mail.ru>
2022-03-21 14:49:38 +0100

    Created userLogin in usersControllers.js

--- src/server/controllers/usersControllers/usersControllers.js
+++ src/server/controllers/usersControllers/usersControllers.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../../../database/models/User");
 
 const getAllUsers = async (req, res) => {
@@ -5,4 +7,28 @@ const getAllUsers = async (req, res) => {
   res.json({ users });
 };
 
-module.exports = getAllUsers;
+const userLogin = async (req, res, next) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    const error = new Error("User not found");
+    error.code = 401;
+    next(error);
+  } else {
+    const correctPassword = await bcrypt.compare(password, user.password);
+    if (!correctPassword) {
+      const error = new Error(`Invalid password for user ${user.username}`);
+      error.code = 401;
+      next(error);
+    } else {
+      const userDate = {
+        username: user.username,
+        id: user.id,
+      };
+      const token = jwt.sign(userDate, process.env.JWT_SECRET);
+      res.json({ token });
+    }
+  }
+};
+
+module.exports = { getAllUsers, userLogin };
